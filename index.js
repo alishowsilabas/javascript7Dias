dfa401d6a108c8fa5f0dba5449018377bfcd4a44
alishowsilabas <alissonsilabas@gmail.com>
2023-09-08 03:38:54 -0300

    Update desafio

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let continuacaoTexto = 'foi feita em JavaScript.';
-let elementoSpan = document.getElementById("continuacaoEmJS");
-elementoSpan.innerHTML = continuacaoTexto;
+let addedText = 'foi adicionado com JavaScript ;D';
+let spanID = document.getElementById("textJS");
+spanID.innerHTML = addedText;
