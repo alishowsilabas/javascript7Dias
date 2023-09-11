2f1b4e84e95ac4353979de7222245c39ca839cdd
alishowsilabas <alissonsilabas@gmail.com>
2023-09-11 15:08:12 -0300

    Studies update

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let productValue = Number(prompt('Enter the product value: '));
 
 if(productValue >= 20){
     document.getElementsByClassName('result')[0].innerHTML = ` Produto aprovado, valor: $${productValue}`;
+    document.getElementsByClassName('result')[0].style.color = 'green'
+} else {
+    document.getElementsByClassName('result')[0].innerHTML = ` Produto reprovado, valor: $${productValue}`;
+    document.getElementsByClassName('result')[0].style.color = 'red'
 }
